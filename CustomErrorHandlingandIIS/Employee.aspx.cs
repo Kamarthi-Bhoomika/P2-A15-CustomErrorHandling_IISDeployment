96e12cb3025337c1936dc8f8e43f21139379033d
Kamarthi-Bhoomika <kbhoomika05@gmail.com>
2023-08-31 15:52:52 +0530

    initial version

--- CustomErrorHandlingandIIS/Employee.aspx.cs
+++ CustomErrorHandlingandIIS/Employee.aspx.cs
@@ -7,14 +7,9 @@ using System.Web.UI.WebControls;
 
 namespace CustomErrorHandlingandIIS
 {
-    public partial class Home : System.Web.UI.Page
+    public partial class Employee : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        protected void Btn_Click(object sender, EventArgs e)
         {
 
         }
--- CustomErrorHandlingandIIS/Employee.aspx.designer.cs
+++ CustomErrorHandlingandIIS/Employee.aspx.designer.cs
@@ -11,7 +11,7 @@ namespace CustomErrorHandlingandIIS
 {
 
 
-    public partial class Home
+    public partial class Employee
     {
 
         /// <summary>
@@ -24,12 +24,21 @@ namespace CustomErrorHandlingandIIS
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
-        /// Btn control.
+        /// GridView1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Btn;
+        protected global::System.Web.UI.WebControls.GridView GridView1;
+
+        /// <summary>
+        /// SqlDataSource1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource1;
     }
 }
